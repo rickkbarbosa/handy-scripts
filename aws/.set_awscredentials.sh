3ceedabda97aa71b9bf9a2d893dafd5afafd7017
Ricardo Barbosa <ricardo.barbosa@nubeliu.com>
2018-01-30 16:10:09 -0200

    Set aws scripts

--- aws/.set_awscredentials.sh
+++ aws/.set_awscredentials.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-AWS_CREDENTIALS_FILE="/home/rickk/.aws/credentials"
-AWS_S3CMD_FILE="/home/rickk/.s3cfg"
+#Getting variables
+HOMEDIR=$(echo ~$(whoami;);)
+AWS_CREDENTIALS_FILE="${HOMEDIR}/.aws/credentials"
+AWS_S3CMD_FILE="${HOMEDIR}/.s3cfg"
 
-sed -i "/^aws_access_key_id.*/d" $AWS_CREDENTIALS_FILE
-sed -i "/^aws_secret_access_key.*/d" $AWS_CREDENTIALS_FILE
+#Starting the environment
+echo Ativando ambiente $(echo ${PWD##*/};)
 
-echo "aws_access_key_id = ${AWS_ACCESS_KEY}" >> $AWS_CREDENTIALS_FILE
-echo "aws_secret_access_key = ${AWS_ACCESS_SECRET_KEY}" >> $AWS_CREDENTIALS_FILE
+#Replacing AWS credentials for general purpose
+sed -i "/^aws_access_key_id.*/d" ${AWS_CREDENTIALS_FILE}
+sed -i "/^aws_secret_access_key.*/d" ${AWS_CREDENTIALS_FILE}
+echo "aws_access_key_id = ${AWS_ACCESS_KEY}" >> ${AWS_CREDENTIALS_FILE}
+echo "aws_secret_access_key = ${AWS_ACCESS_SECRET_KEY}" >> ${AWS_CREDENTIALS_FILE}
 
-sed -i "/^access_key.*/d" $AWS_S3CMD_FILE
-sed -i "/^secret_key.*/d" $AWS_S3CMD_FILE
-
-echo "access_key = ${AWS_ACCESS_KEY}" >> $AWS_S3CMD_FILE
-echo "secret_key = ${AWS_ACCESS_SECRET_KEY}" >> $AWS_S3CMD_FILE
+#Replacing AWS credentials for s3cmd
+sed -i "/^access_key.*/d" ${AWS_S3CMD_FILE}
+sed -i "/^secret_key.*/d" ${AWS_S3CMD_FILE}
+echo "access_key = ${AWS_ACCESS_KEY}" >> ${AWS_S3CMD_FILE}
+echo "secret_key = ${AWS_ACCESS_SECRET_KEY}" >> ${AWS_S3CMD_FILE}
 
 #Setting global variables 
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
-export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY}
+export AWS_SECRET_ACCESS_KEY=${AWS_ACCESS_SECRET_KEY}
 #export AWS_DEFAULT_REGION=$AWS_REGION
