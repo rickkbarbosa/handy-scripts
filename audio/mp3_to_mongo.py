9ab2162d91cd8013029cc222573d76884bca9911
Ricardo Barbosa <ricardo.barbosa@nubeliu.com>
2020-05-26 14:46:01 -0300

    Adding DynamoDB and updating

--- audio/mp3_to_mongo.py
+++ audio/mp3_to_mongo.py
@@ -1,45 +1,78 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/python
-#===============================================================================
+# ===============================================================================
 # IDENTIFICATION DIVISION
 #        ID SVN:   $Id$
 #          FILE:  mp3_to_mongo.py
 #         USAGE:  ---
-#   DESCRIPTION:  Find mp3 files, read your ID3tags and upload information to MongoDB collection
+#   DESCRIPTION:  Find mp3 files, read your ID3tags and upload information to MongoDB collection or AWS DynamoDB Table
 #       OPTIONS:  ---
-#  REQUIREMENTS:  pymongo (apt-get install python-pymongo)
-#			  python-idtag (apt-get install python-id3 // yum install ftp://ftp.pbone.net/mirror/archive.fedoraproject.org/fedora/linux/releases/14/Everything/x86_64/os/Packages/python-id3-1.2-16.fc14.noarch.rpm
-#                                 
-#				  findtools (pip install findtools)
+#  REQUIREMENTS:  mutagen
+#                 pymongo
+#
 #          BUGS:  ---
 #         NOTES:  ---
 #          TODO:  ---
 #        AUTHOR:  Ricardo Barbosa (Rickk Barbosa), rbarbosa@advertiser.com.br
-#       COMPANY:  Advertiser Technologies
-#       VERSION:  1.0
+#       COMPANY:  ---
+#       VERSION:  2.0
 #       CREATED:  12/08/2014 09:58:30 AM BRT
-#      REVISION:  ---
-#===============================================================================
+#      REVISION:  26/05/2020 01:05:20 AM BRT
+# ===============================================================================
 
-from findtools.find_files import (find_files, Match)
-from ID3 import *
-from pymongo import *
+import boto3
+import pymongo
+import os
+from mutagen.easyid3 import EasyID3
 
-mp3_files_pattern = Match(filetype='f', name='*.mp3')
-found_files = find_files(path='/root/Music', match=mp3_files_pattern)
+if(sys.version_info.major >= 3):
+    import importlib
+    importlib.reload(sys)
+else:
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
 
-conn = MongoClient(host="mongodbR")
-music = conn.music
-tags = music.tags
 
+music_path = "/root/Music"
+music_collectionname = "music"
+mongodb_host = "mongodb://localhost:27017/"
+
+
+''' Searching for MP3 Files '''
+found_files = list()
+for root, directories, filenames in os.walk(music_path):
+    for filename in filenames:
+        if '.mp3' in filename:
+            found_files.append(os.path.join(root, filename))
+
+
+''' MongoDB Connect '''
+conn = pymongo.MongoClient(mongodb_host)
+music = conn[music_collectionname]
+mp3_tags = music.tags
 tags.create_index('filename')
 
+
+''' Filling List '''
+tags = []
 for found_file in found_files:
-        tagmp3 = {}
-        id3info = ID3(found_file)
-        fileName = id3info.file.name.decode('utf-8','ignore')
+    tagmp3 = {}
+    try:
+        ''' Reading tags '''
+        id3info = EasyID3(found_file)
         for item in id3info.items():
-                tagmp3[item[0].lower().decode("utf-8", "ignore")] = item[1].decode("utf-8","ignore")
-        fileName = id3info.file.name.decode('utf-8','ignore')
-        tagmp3['filename'] = fileName
-        tags.update({'filename':fileName},tagmp3,upsert=True)
+            tagmp3[item[0].lower()] = item[1][0]
+            ''' Filling NoSQL '''
+            # mp3_tags.update({'filename':found_file},tagmp3,upsert=True)       #MongoDB
+        tags.append(tagmp3)
+    except Exception as e:
+        print("")
+
+
+''' When DynamoDB '''
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(music_collectionname)
+
+for i in range(len(tags)):
+    table.put_item(
+        Item=tags[i]
+    )
