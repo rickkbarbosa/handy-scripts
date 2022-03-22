5131ca2fbd5909aa322359ef8536a0f8c135f001
Rickk Barbosa <rickkbarbosa@users.noreply.github.com>
2022-03-22 00:55:38 -0300

    Update irpf.sh

--- irpf.sh
+++ irpf.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-YEAR=2020
-IRPF_VERSION="1.4"
+YEAR=$(date +"%Y";)
+IRPF_VERSION="1.1"
 
-wget "http://downloadirpf.receita.fazenda.gov.br/irpf/${YEAR}/irpf/arquivos/IRPF${YEAR}-1.4.zip"
+wget -k "https://downloadirpf.receita.fazenda.gov.br/irpf/${YEAR}/irpf/arquivos/IRPF${YEAR}-${IRPF_VERSION}.zip"
 
 sudo unzip IRPF${YEAR}-${IRPF_VERSION}.zip -d /opt/ 
-echo -e '[Desktop Entry]\n Version=1.0\n Name=irpf2016\n Exec=java -Xms128M -Xmx512M -jar /opt/IRPF2020/irpf.jar\n Icon=/\n Type=Application\n Categories=Application' | sudo tee "/usr/share/applications/irpf${YEAR}.desktop"
+echo -e "[Desktop Entry]\n Version=1.0\n Name=irpf${YEAR}\n Exec=java -Xms128M -Xmx512M -jar /opt/IRPF${YEAR}/irpf.jar\n Icon=/\n Type=Application\n Categories=Application" | sudo tee "/usr/share/applications/irpf${YEAR}.desktop"
 sudo chmod +x "/usr/share/applications/irpf${YEAR}.desktop"
-ln -s "/usr/share/applications/irpf${YEAR}.desktop" ~/Desktop/
+
+LINUX_USERS=$(egrep ':x:100[0-9]' /etc/passwd | cut -d ':' -f 6;)
+for USER in $LINUX_USERS; do 
+  ln -s "/usr/share/applications/irpf${YEAR}.desktop" ${USER}/Desktop/ 
+done
