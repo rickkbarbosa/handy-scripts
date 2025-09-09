7346a879a5d9dc4cd04d7cb10adbb0958d1c8f6c
Rickk Barbosa <ricardo@senseinode.com>
2025-09-09 19:19:56 -0300

    terraform - rover

--- terraform_tools/terraform_image.sh
+++ terraform_tools/terraform_image.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 ### Create a visualization on terraform plan
 ### https://github.com/im2nguyen/rover
-HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd ${HERE}
 terraform plan -out=plan.out
 terraform show -json plan.out > plan.json
 docker run --rm -it -p 9000:9000 -v $(pwd)/plan.json:/src/plan.json im2nguyen/rover:latest -planJSONPath=plan.json
